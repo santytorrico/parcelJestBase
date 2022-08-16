4c7743c4ce1da5ea4b659bd8d18b7b6880cb2c39
santytorrico <90476969+santytorrico@users.noreply.github.com>
2022-08-16 11:54:07 -0400

    Added multiply function

---
+++ src/operator.js
@@ -0,0 +1,7 @@
+function sumar(a, b) {
+  return a + b;
+}
+function multiply(a,b){
+  return a * b;
+}
+export {sumar, multiply};
--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,4 @@
-import sumar from "./sumador";
+import {sumar,multiply} from "./operator";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
@@ -12,4 +12,18 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+const first2 = document.querySelector("#first-number");
+const second2 = document.querySelector("#second-number");
+const form2 = document.querySelector("#multiplicar-form");
+const div2 = document.querySelector("#resultado-div2");
+
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstn = Number.parseInt(first2.value);
+  const secondn = Number.parseInt(second2.value);
+  
+  div2.innerHTML = "<p>" + multiply(firstn,secondn) + "</p>";
 });
